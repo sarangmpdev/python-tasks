e9474ec1c4fd272c10aa6f0d68e960a2ce5e1dde
sarangmpdev <sarangmp.coder@gmail.com>
2025-08-11 16:37:30 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,17 @@
+num1=input("Enter the first number")
+num2=input("Enter the second number")
+
+num1=int(num1)
+num2=int(num2)
+
+addition=num1+num2
+print("Addition :",addition)
+
+subtraction=num1-num2
+print("Subtraction :",subtraction)
+
+multiplication=num1*num2
+print("Multiplication :",multiplication)
+
+division=num1/num2
+print("Division :",division)
---
+++ task2.py
@@ -0,0 +1,6 @@
+first=input("Enter your first name: ")
+last=input("Enter your last name: ")
+
+fullname=first+" "+last
+
+print(f"Hey, {fullname} welcome to the python program...")
